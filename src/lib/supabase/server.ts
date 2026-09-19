2c05a8753d1cf6c6297ee1ea2e04f873ca882f8c
MichaelKazembe <michaelkazembe@gmail.com>
2026-09-19 03:55:12 +0200

    TASK: Login functionality implemented

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,8 +6,8 @@ export async function createClient() {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.GEARSPHERE_DATABASE_URL!,
-    process.env.GEARSPHERE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
